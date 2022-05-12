6ecc1edca2aae0ce760981753fbbdb75e2e70988
vitoriamj <vitoria.m.justino@gmail.com.br>
2022-05-12 11:43:08 -0300

    Feito a interface do aplicativo de calculadora

---
+++ App.js
@@ -0,0 +1,82 @@
+import React, {Component} from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+import Button from './src/components/Button';
+import Display from './src/components/Display';
+
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
+export default class App extends Component {
+  state = {
+    initialState,
+  };
+
+  addDigit = n => {
+    if (n === '.' && this.state.displayValue.includes('.')) {
+      return;
+    }
+
+    const clearDisplay =
+      this.state.displayValue === '0' || this.state.clearDisplay;
+
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+
+    const displayValue = currentValue + n;
+    this.setState({displayValue, clearDisplay: false});
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue);
+      const values = [this.state.values];
+      values[this.state.current] = newValue;
+    }
+  };
+
+  clearMemory = () => {
+    this.setState({displayValue: '0'});
+  };
+
+  setOperation = operator => {};
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Display value={this.state.displayValue}></Display>
+        <SafeAreaView style={styles.buttons}>
+          <Button label="AC" triple onClick={this.clearMemory} />
+          <Button label="/" operation onClick={this.setOperation('/')} />
+          <Button label="7" onClick={this.addDigit} />
+          <Button label="8" onClick={this.addDigit} />
+          <Button label="9" onClick={this.addDigit} />
+          <Button label="*" operation onClick={this.setOperation} />
+          <Button label="4" onClick={this.addDigit} />
+          <Button label="5" onClick={this.addDigit} />
+          <Button label="6" onClick={this.addDigit} />
+          <Button label="-" operation onClick={this.setOperation} />
+          <Button label="1" onClick={this.addDigit} />
+          <Button label="2" onClick={this.addDigit} />
+          <Button label="3" onClick={this.addDigit} />
+          <Button label="+" operation onClick={this.setOperation} />
+          <Button label="0" double onClick={this.addDigit} />
+          <Button label="." onClick={this.addDigit} />
+          <Button label="=" onClick={this.setOperation} />
+        </SafeAreaView>
+      </SafeAreaView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+});
